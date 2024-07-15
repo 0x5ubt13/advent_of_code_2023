45b78083fe334aa378a148c7c55051cf2e995541
0x5utb13 <5ubt13@protonmail.com>
2024-07-15 12:59:15 +0100

    playing around with some ideas

--- Day_04/day_04.go
+++ Day_04/day_04.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -23,30 +22,54 @@ func main() {
 		"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
 		"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"}
 
-	shadowMap := make(map[int]string)
+	parsedMap := make(map[int][]string)
 	for _, card := range testData {
 		cardSlice := strings.Split(card, ":")[0]
 		numbersSlice := strings.Split(card, ":")[1]
-		_, key := strings.Split(cardSlice, " ")[1]
-
-		shadowmap[] = strings.Split(numbersSlice, "|")
-	}
-
-	keySlice := make([]int, 0)
-	for key := range shadowMap {
-		num, err := strconv.Atoi(key)
+		num, err := strconv.Atoi(strings.Split(cardSlice, " ")[1])
 		if err != nil {
-			fmt.Printf("Error: %s\n", err)
+			log.Fatal(err)
 		}
-		keySlice = append(keySlice, num)
+		parsedMap[num] = strings.Split(numbersSlice, "|")
 	}
 
-	sort.Ints(keySlice)
+	score := getScore(parsedMap)
+	fmt.Println(score)
 
-	for _, key := range keySlice {
-		fmt.Printf("Key: %d, Value: %s\n", key, shadowMap[key])
+	// Sort and print ordered
+	//keySlice := make([]int, 0)
+	//for key := range parsedMap {
+	//	num, err := strconv.Atoi(key)
+	//	if err != nil {
+	//		fmt.Printf("Error: %s\n", err)
+	//	}
+	//	keySlice = append(keySlice, num)
+	//}
+	//
+	//sort.Ints(keySlice)
+	//
+	//for _, key := range keySlice {
+	//	fmt.Printf("Key: %d, Value: %s\n", key, parsedMap[key])
+	//}
+
+}
+
+func getScore(parsedMap map[int][]string) int {
+	//testData := []string{
+	//	"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+	//	"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+	//	"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+	//	"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+	//	"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+	//	"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"}
+	for _, value := range parsedMap {
+		for _, num := range value[0] {
+			fmt.Println(value)
+			fmt.Println(value[0])
+			fmt.Printf("%s\n", strconv.Itoa(int(num)))
+		}
 	}
-
+	return 0
 }
 
 func getInput(filename string) []int64 {
